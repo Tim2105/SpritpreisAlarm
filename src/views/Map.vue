1237019c2a6b18d012c7247898295163f65bf8ff
Tim Plotzki <plotzki.tim@fh-swf.de>
2023-02-23 15:59:41 +0100

    Tankstellen werden jetzt auf der Karte angezeigt

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,6 +1,9 @@
 <template>
-       <!-- ein Menu was auf klick die Punkte Preise und favoriten  anzeigt -->
-  
+    <!--Einbinden der Css Datei von Leaflet -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+     integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+     crossorigin=""/>
+     <!--Menü Icon -->
     <ion-menu content-id="main-content">
         <ion-header>
             <ion-toolbar>
@@ -14,7 +17,7 @@
         </ion-content>
     </ion-menu>
 
-    <ion-page id="main-content">    
+    <ion-page id="main-content">
         <ion-content class="ion-padding">
             <div id="map"></div>
             <ion-menu-toggle>
@@ -23,12 +26,12 @@
         </ion-content>
 
     </ion-page>
-        
-
 </template>
 
 <script lang="ts">
-    import {
+
+//Einbinden der Ionic Bibliothek
+import {
     IonButton,
     IonContent,
     IonHeader,
@@ -37,22 +40,25 @@
     IonPage,
     IonTitle,
     IonToolbar
-  } from '@ionic/vue';
+} from '@ionic/vue';
 
-  import { defineComponent } from 'vue';
-  import { Options, Vue } from 'vue-class-component';
-    import APIRequest from '@/models/logic/APIRequest';
-    import Station from '@/models/dao/Station';
-
-  
-
-    // einbindungen für die Karte (leaflet)
-    import L from 'leaflet';
-    import 'leaflet/dist/leaflet.css';
-    import 'leaflet/dist/images/marker-shadow.png';
+//Einbinden der Vue Bibliothek
+import { defineComponent } from 'vue';
+import { Options, Vue } from 'vue-class-component';
+import APIRequest from '@/models/logic/APIRequest';
+import Station from '@/models/dao/Station';
 
 
-    @Options({components:
+
+//Einbinden der Leaflet Bibliothek
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.js';
+import 'leaflet/dist/images/marker-shadow.png';
+import 'leaflet/dist/images/marker-icon.png';
+
+@Options({
+    components:
     {
         IonButton,
         IonContent,
@@ -61,45 +67,48 @@
         IonMenuToggle,
         IonPage,
         IonTitle,
-        IonToolbar         
+        IonToolbar
     }
-    })
+})
 
-    export default class Map extends Vue {
+export default class Map extends Vue {
 
-        public async mounted() : Promise<void> {
+    public async mounted(): Promise<void> {
 
-            const apiRequest : APIRequest = await APIRequest.fromCurrentLocation();
-            const stations : Station[] = await apiRequest.getStations();
-            console.log(stations);
-
-
-            // Karte initialisieren mit Koordinaten des Users
-            const map = L.map('map').setView([apiRequest.coordinate.latitude , apiRequest.coordinate.longitude], 13);
-            const marker_01 = L.marker([apiRequest.coordinate.latitude , apiRequest.coordinate.longitude]).addTo(map);
-
-            var marker = L.marker([apiRequest.coordinate.latitude , apiRequest.coordinate.longitude]).addTo(map);
-            // Kartenlayer hinzufügen wo sich der user befindet
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-            marker_01.bindPopup("You are here").openPopup();
-
-            // Marker für die Tankstellen hinzufügen
-            for (let i = 0; i < stations.length; i++) {
-                const station : Station = stations[i];
-                const marker = L.marker([station.coordinate.latitude, station.coordinate.longitude]).addTo(map);
-                marker.bindPopup(station.name).openPopup();
-            }
+        const apiRequest: APIRequest = await APIRequest.fromCurrentLocation();
+        const stations: Station[] = await apiRequest.getStations();
+        console.log(stations);
 
 
+        // Karte initialisieren mit Koordinaten des Users und eigenem Icon
+        const map = L.map('map').setView([apiRequest.coordinate.latitude, apiRequest.coordinate.longitude], 13);
+
+        // Icon für den User
+        var myLocation = L.icon({
+            iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/images/marker-icon-2x.png',
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],            
+        });
+
+        // Kartenlayer hinzufügen wo sich der user befindet
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        // Marker für die Tankstellen hinzufügen mit eigenem Icon
+        for (let i = 0; i < stations.length; i++) {
+            const station: Station = stations[i];
+            L.marker([station.coordinate.latitude, station.coordinate.longitude]).addTo(map).bindPopup(station.name + "<br>" + station.address).openPopup();
         }
 
+        // Marker für den User hinzufügen mit eigenem Icon
+        L.marker([apiRequest.coordinate.latitude, apiRequest.coordinate.longitude], {icon: myLocation}).addTo(map).bindPopup("You are here").openPopup();
     }
 
+}
 
-    
+
+
 </script>
 
 <style lang="scss">
@@ -107,15 +116,12 @@
     width: 100%;
     height: 100%;
 }
+
 //soll über der Karte angezeigt werden
-.Menu_button{
+.Menu_button {
     position: fixed;
     top: 0;
     right: 0;
     z-index: 1000;
-   
-  
-
-    
 }
 </style>
