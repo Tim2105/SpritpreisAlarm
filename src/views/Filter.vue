91ebf1eac43c6a2dd760ab1a3e1769f556a51484
Tim Plotzki <plotzki.tim@fh-swf.de>
2023-03-10 12:21:12 +0100

    Bugfixes

--- src/views/Filter.vue
+++ src/views/Filter.vue
@@ -2,7 +2,7 @@
         <ion-item>
             <ion-label>Filter</ion-label>
             <!-- Hier wird die Auswahl der Spritart gemacht  mehrfache Auswahl möglich-->
-            <ion-select multiple="true" interface="popover" placeholder="Filtern" @ionChange ="filter()">
+            <ion-select v-model="filterPredicate" multiple="true" interface="popover" placeholder="Filtern" @ionChange ="filter()">
                 <ion-select-option value="Open">Geöffnet</ion-select-option>
                 <ion-select-option value="Diesel">Diesel</ion-select-option>
                 <ion-select-option value="E10">E10</ion-select-option>
@@ -35,7 +35,7 @@ import { Options, Vue } from 'vue-class-component';
 
 export default class FilterVue extends Vue {
  
-  public _filter: any = [];
+  public _filterPredicate: any = [];
   private _isOpen: boolean = true;
   private _isDiesel: boolean = true;
   private _isE10: boolean = true;
@@ -43,13 +43,22 @@ export default class FilterVue extends Vue {
 
 
   public async filter(): Promise<void> {
-
     //Prüfen ob die Checkboxen angeklickt sind und die Variable entsprechend setzen
     await Filter_ts.filterStations(this._isDiesel, this._isE5, this._isE10 ,this._isOpen);
     this.$emit ('update');
   }
 
- 
+  public get filterPredicate(): any {
+    return this._filterPredicate;
+  }
+
+  public set filterPredicate(value: any) {
+    this._filterPredicate = value;
+    this._isOpen = this._filterPredicate.includes('Open');
+    this._isDiesel = this._filterPredicate.includes('Diesel');
+    this._isE10 = this._filterPredicate.includes('E10');
+    this._isE5 = this._filterPredicate.includes('E5');
+  }
 }
 
 
